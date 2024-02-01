a14926653dd556882219f046c0558cadbccef546
Aldas Žarnauskas <aldas02027@gmail.com>
2024-02-01 16:49:36 +0000

    data analysis version 2

--- data_analysis.r
+++ data_analysis.r
@@ -1,36 +1,57 @@
+# Author: Aldas Žarnauskas
+# Title: Analysis of Taxonomic Abundance in Infant Samples
+# Date: 21/007/2023
+# Last time updated: 21/007/2023
+# Objective: investigate if previous antibiotic exposure affects alpha diversity and
+# microbial composition of 12 month old infant samples
+
+### ------------------------- Cleaning Environment -------------------------- ##
+################################################################################
 rm(list = ls(all.names = T))
 gc()
 
+### ------------------------------- Libraries ------------------------------- ##
+################################################################################
 library(tidyverse)
 library(phyloseq)
 library(vegan)
+library('knitr')
 
 set.seed(199402)
 
+### ----------------------- Data Import + Wrangling ------------------------- ##
+################################################################################
+# Loading metadata and taxonomic abundance table
 load("candidate_assignment.Rdata.rdata")
+# Rounding taxonomic abundance values rounded upwards to the nearest integer
 taxtable <- apply(taxtable %>% as.matrix(), 2, ceiling) %>% t()
+# Convert taxtable into the otu_table object
 otu <- otu_table(taxtable, taxa_are_rows=T)
 
+# Set sample IDs to rownames
 metadata <- meta %>% column_to_rownames(var = "sample_name") 
-metadata_filt <- metadata %>% 
-  {.$Age_months <- ifelse(.$Age == "1 month", 1,
-                          ifelse(.$Age == "6 months", 6, 12));.} %>% 
-  sample_data()
 
-#------------------------- Antibiotic Exposure
+#--------------------------------- Antibiotic Exposure ------------------------#
+# Select rows where Age is 1 month
 metadata_1 <- metadata %>% .[.$Age == "1 month",] %>% 
   {.$Antibiotic_1 <- .$Antibiotic; .} %>% 
   dplyr::select(Participant_id, Antibiotic_1)
+# Select rows where Age is 6 months
 metadata_6 <- metadata %>% .[.$Age == "6 months",] %>% 
   {.$Antibiotic_6 <- .$Antibiotic; .} %>% 
   dplyr::select(Participant_id, Antibiotic_6)
+# Select rows where Age is 12 months
 metadata_12 <- metadata %>% .[.$Age == "12 months",] %>% 
   {.$Antibiotic_12 <- .$Antibiotic; .} %>% 
   dplyr::select(Participant_id, Antibiotic_12)
 
-metadata_1$Participant_id == metadata_6$Participant_id
-metadata_1$Participant_id == metadata_12$Participant_id
+# Quality inspection. Check if participant_id is present in above three objects
+all(metadata_1$Participant_id == metadata_6$Participant_id)
+all(metadata_1$Participant_id %in% metadata_6$Participant_id)
+all(metadata_1$Participant_id == metadata_12$Participant_id)
+all(metadata_1$Participant_id %in% metadata_6$Participant_id)
 
+# Join all three data frames to have patient's antibiotic exposure history on a single row 
 metadata_join <- full_join(metadata_1, metadata_6, by = "Participant_id") %>% 
   full_join(., metadata_12, by = "Participant_id") %>% 
   {.$antibiotic_exposure <- ifelse(.$Antibiotic_1 == 1, "Exposed_at_1m", ifelse(
@@ -39,25 +60,32 @@ metadata_join <- full_join(metadata_1, metadata_6, by = "Participant_id") %>%
   )); .} %>% 
   dplyr::select(Participant_id, antibiotic_exposure)
 
+# Add new columns showing patients antibiotic exposure to a metadata
 metadata_new <- metadata %>% left_join(., metadata_join, by = "Participant_id") %>%
   {rownames(.) <- rownames(metadata); .} %>% 
-  sample_data()
+  sample_data() # convert into sample_data object
 
-#------------------------ Phyloseq object construction and analysis
+###---------------------- Phyloseq Construction and Analysis ----------------###
+################################################################################
 physeq <- phyloseq(otu, metadata_new)
+rm(taxtable)
+# Subset rows where Age = 12 months
 physeq_12m <- subset_samples(physeq, Age == "12 months")
 
+# Estimate samples' alpha diversity
 richness <- estimate_richness(physeq_12m)
 
-plot_richness(physeq_12m, x = "antibiotic_exposure"
+# Plot selected richness metrics
+plot_richness(physeq_12m, x = "antibiotic_exposure",
               measures = c("Observed", "Chao1", "Shannon"),
               color = "antibiotic_exposure") + theme_classic() +
   theme(axis.text.x = element_text(angle = 90))
   
-
-
+# Transform data into relative abundance
 physeq_12m.transf <- transform_sample_counts(physeq_12m, function(x) x/sum(x))
+# Perform PCoA using Bray-Curtis dissimilarities
 ord.meas <- ordinate(physeq_12m.transf, method = "PCoA", distance = "bray")
+# Plot a PCoA of Bray-Curtis dissimilarity meassures
 p <- plot_ordination(physeq_12m.transf, ord.meas, color = "antibiotic_exposure") +
   geom_point(size=2) + 
   theme_classic() + 
@@ -65,161 +93,87 @@ p <- plot_ordination(physeq_12m.transf, ord.meas, color = "antibiotic_exposure")
         legend.position = "right") +
   ggtitle("PCoA: Bray-Curtis")
 
-#------------------------ Adonis
-library('knitr')
+###------------------------------- PERMANOVA: adonis2 -----------------------###
+################################################################################
 
-
-
+#------------------------------------- All Groups PERMANOVA -------------------#
+# Calculate Bray-Curtis dissimilarities for infant samples
 dist.meas <- phyloseq::distance(physeq_12m.transf, method = "bray")
+
+# Use PERMANOVA to indentify if at least one group differes in Bray-Curtis dissimilarities
 bray <- adonis2(dist.meas ~ 
                        sample_data(physeq_12m.transf)$antibiotic_exposure, 
                      permutations = 999, method = "bray")
-adonis.res
-
-
-
-
-
-# Observed, Chao1 (richness), and Shannon (diversity)
+# Use PERMANOVA to indentify if at least one group differes in observed diversity
 Observed <- adonis2(richness[["Observed"]] ~ sample_data(physeq_12m.transf)$antibiotic_exposure,
                  data = metadata,
                  permutations = 999)
+# Use PERMANOVA to indentify if at least one group differes in Chao1 richness
 Chao1 <- adonis2(richness[["Chao1"]] ~ sample_data(physeq_12m.transf)$antibiotic_exposure,
         data = metadata,
         permutations = 999)
+# Use PERMANOVA to indentify if at least one group differes in Shannon diversity
 Shannon <- adonis2(richness[["Shannon"]] ~ sample_data(physeq_12m.transf)$antibiotic_exposure,
                  data = metadata,
                  permutations = 999)
 
-
-pairwise_adonis <- numeric()
-
-# Ex1m vs Ex6m
+#-------------------------------- Pairwise Comparison -------------------------#
+# Select rows where antibiotic_exposure is Ex1m or Ex6m
 Ex1m_Ex6m <- sample_data(physeq_12m.transf) %>% as.data.frame() %>% 
   .[.$antibiotic_exposure == "Exposed_at_1m" | .$antibiotic_exposure == "Exposed_at_6m",]
-# Ex1m vs Ex12m
+# Select rows where antibiotic_exposure is Ex1m or Ex12m
 Ex1m_Ex12m <- sample_data(physeq_12m.transf) %>% as.data.frame() %>%
   .[.$antibiotic_exposure == "Exposed_at_1m" | .$antibiotic_exposure == "Exposed_at_12m",]
-# Ex1m vs ExNo
+# Select rows where antibiotic_exposure is Ex1m or ExNo
 Ex1m_ExNo <- sample_data(physeq_12m.transf) %>% as.data.frame() %>%
   .[.$antibiotic_exposure == "Exposed_at_1m" | .$antibiotic_exposure == "No_exposure",]
-# Ex6m vs Ex12m
+# Select rows where antibiotic_exposure is Ex6m or Ex12m
 Ex6m_Ex12m <- sample_data(physeq_12m.transf) %>% as.data.frame() %>%
   .[.$antibiotic_exposure == "Exposed_at_6m" | .$antibiotic_exposure == "Exposed_at_12m",]
-# Ex6m vs ExNo
+# Select rows where antibiotic_exposure is Ex6m or ExNo
 Ex6m_ExNo <- sample_data(physeq_12m.transf) %>% as.data.frame() %>%
   .[.$antibiotic_exposure == "Exposed_at_6m" | .$antibiotic_exposure == "No_exposure",]
-# Ex12m vs ExNo
+# Select rows where antibiotic_exposure is Ex12m or ExNo
 Ex12m_ExNo <- sample_data(physeq_12m.transf) %>% as.data.frame() %>%
   .[.$antibiotic_exposure == "Exposed_at_12m" | .$antibiotic_exposure == "No_exposure",]
 
+# Store F values from the pairwise comparisons
+pairwise_adonis <- numeric()
 
-pairwise_adonis["Chao1_Ex1m_Ex6m"] <- adonis2(
-  richness["Chao1"][rownames(Ex1m_Ex6m),] ~ Ex1m_Ex6m$antibiotic_exposure,
-        permutations = 999) %>% .[["Pr(>F)"]]
+# Store the pair names
+pairs <- ls()[4:9]
 
-pairwise_adonis["Chao1_Ex1m_Ex12m"] <- adonis2(
-  richness["Chao1"][rownames(Ex1m_Ex12m),] ~ Ex1m_Ex12m$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
+# pairwise_comparison performs pairwise analysis of variance
+pairwise_comparison <- function(value_type, values, pair_list){
+  
+  # Stores pairwise comparison F values
+  pairwise_adonis <- numeric()
+  
+  # Iteratively performs pairwise comparison for each pair
+  for (pair in pair_list){
+    # Stores the value
+    name <- paste(value_type, pair, sep = "_")
+    pair <- get(pair)
+    
+    if (value_type == "Bray"){
+      
+      values <- values %>% as.matrix()
+      pairwise_adonis[name] <- adonis2(values[rownames(
+        pair),rownames(pair)] ~ pair$antibiotic_exposure, permutations = 999) %>% 
+        .[["Pr(>F)"]] 
+      
+    } else{
+      
+      pairwise_adonis[name] <- adonis2(values[value_type][rownames(
+        pair),] ~ pair$antibiotic_exposure, permutations = 999) %>% 
+        .[["Pr(>F)"]] 
+      
+    }
+  } 
 
-pairwise_adonis["Chao1_Ex1m_ExNo"] <- adonis2(
-  richness["Chao1"][rownames(Ex1m_ExNo),] ~ Ex1m_ExNo$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
+  return(pairwise_adonis)
+  
+}
 
-pairwise_adonis["Chao1_Ex6m_Ex12m"] <- adonis2(
-  richness["Chao1"][rownames(Ex6m_Ex12m),] ~ Ex6m_Ex12m$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Chao1_Ex6m_ExNo"] <- adonis2(
-  richness["Chao1"][rownames(Ex6m_ExNo),] ~ Ex6m_ExNo$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Chao1_Ex12m_ExNo"] <- adonis2(
-  richness["Chao1"][rownames(Ex12m_ExNo),] ~ Ex12m_ExNo$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-
-
-
-pairwise_adonis["Observed_Ex1m_Ex6m"] <- adonis2(
-  richness["Observed"][rownames(Ex1m_Ex6m),] ~ Ex1m_Ex6m$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Observed_Ex1m_Ex12m"] <- adonis2(
-  richness["Observed"][rownames(Ex1m_Ex12m),] ~ Ex1m_Ex12m$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Observed_Ex1m_ExNo"] <- adonis2(
-  richness["Observed"][rownames(Ex1m_ExNo),] ~ Ex1m_ExNo$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Observed_Ex6m_Ex12m"] <- adonis2(
-  richness["Observed"][rownames(Ex6m_Ex12m),] ~ Ex6m_Ex12m$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Observed_Ex6m_ExNo"] <- adonis2(
-  richness["Observed"][rownames(Ex6m_ExNo),] ~ Ex6m_ExNo$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Observed_Ex12m_ExNo"] <- adonis2(
-  richness["Observed"][rownames(Ex12m_ExNo),] ~ Ex12m_ExNo$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-
-
-
-# Shannon
-pairwise_adonis["Shannon_Ex1m_Ex6m"] <- adonis2(
-  richness["Shannon"][rownames(Ex1m_Ex6m),] ~ Ex1m_Ex6m$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Shannon_Ex1m_Ex12m"] <- adonis2(
-  richness["Shannon"][rownames(Ex1m_Ex12m),] ~ Ex1m_Ex12m$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Shannon_Ex1m_ExNo"] <- adonis2(
-  richness["Shannon"][rownames(Ex1m_ExNo),] ~ Ex1m_ExNo$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Shannon_Ex6m_Ex12m"] <- adonis2(
-  richness["Shannon"][rownames(Ex6m_Ex12m),] ~ Ex6m_Ex12m$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Shannon_Ex6m_ExNo"] <- adonis2(
-  richness["Shannon"][rownames(Ex6m_ExNo),] ~ Ex6m_ExNo$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Shannon_Ex12m_ExNo"] <- adonis2(
-  richness["Shannon"][rownames(Ex12m_ExNo),] ~ Ex12m_ExNo$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-
-dist.meas <- dist.meas %>% as.matrix()
-# Bray
-bray <- adonis2(dist.meas ~ 
-                  sample_data(physeq_12m.transf)$antibiotic_exposure, 
-                permutations = 999, method = "bray")
-
-pairwise_adonis["Bray_Ex1m_Ex6m"] <- adonis2(
-  dist.meas[rownames(Ex1m_Ex6m),rownames(Ex1m_Ex6m)] ~ Ex1m_Ex6m$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Bray_Ex1m_Ex12m"] <- adonis2(
-  richness["Shannon"][rownames(Ex1m_Ex12m),] ~ Ex1m_Ex12m$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Bray_Ex1m_ExNo"] <- adonis2(
-  richness["Shannon"][rownames(Ex1m_ExNo),] ~ Ex1m_ExNo$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Bray_Ex6m_Ex12m"] <- adonis2(
-  richness["Shannon"][rownames(Ex6m_Ex12m),] ~ Ex6m_Ex12m$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Bray_Ex6m_ExNo"] <- adonis2(
-  richness["Shannon"][rownames(Ex6m_ExNo),] ~ Ex6m_ExNo$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
-
-pairwise_adonis["Bray_Ex12m_ExNo"] <- adonis2(
-  richness["Shannon"][rownames(Ex12m_ExNo),] ~ Ex12m_ExNo$antibiotic_exposure,
-  permutations = 999) %>% .[["Pr(>F)"]]
+Chao1_pairwise <- pairwise_comparison("Chao1", richness, pairs)
+Bray_pairwise <- pairwise_comparison("Bray", dist.meas, pairs)
